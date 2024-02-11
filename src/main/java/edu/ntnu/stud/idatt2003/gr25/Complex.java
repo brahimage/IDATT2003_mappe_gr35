fcbd097357a6d981c4fb33e8de5f740995cc592d
Wizard764 <150451202+Wizard764@users.noreply.github.com>
2024-02-11 23:09:10 +0100

    Added arithmetic methods for addition, subtraction and multiplication(with scaler) to Complex.

--- src/main/java/edu/ntnu/stud/idatt2003/gr25/Complex.java
+++ src/main/java/edu/ntnu/stud/idatt2003/gr25/Complex.java
@@ -11,6 +11,42 @@ public class Complex extends Vector2D {
         this.imaginaryPart = imaginaryPart;
     }
 
+    /**
+     * Adds the specified complex number to this complex number.
+     *
+     * @param in the complex number to be added to this complex number.
+     * @return a new complex number representing the sum of this complex number and the specified complex number.
+     */
+    public Complex add(Complex in) {
+        double real = realPart + in.realPart;
+        double imag = imaginaryPart + in.imaginaryPart;
+        return new Complex(real, imag);
+    }
+
+    /**
+     * Subtracts the specified complex number from this complex number.
+     *
+     * @param in the complex number to be subtracted from this complex number.
+     * @return a new complex number representing the difference between this complex number and the specified complex number.
+     */
+    public Complex subtract(Complex in) {
+        double real = realPart - in.realPart;
+        double imag = imaginaryPart - in.imaginaryPart;
+        return new Complex(real, imag);
+    }
+
+    /**
+     * Multiplies this complex number by the specified scalar value.
+     *
+     * @param scaler the scalar value to multiply this complex number by.
+     * @return a new complex number representing the product of this complex number and the specified scalar value.
+     */
+    public Complex multiply(float scaler) {
+        double real = realPart * scaler;
+        double imag = imaginaryPart * scaler;
+        return new Complex(real, imag);
+    }
+
     public Complex sqrt() {
         double x0 = Math.sqrt(realPart);
         double x1 = Math.sqrt(imaginaryPart);
