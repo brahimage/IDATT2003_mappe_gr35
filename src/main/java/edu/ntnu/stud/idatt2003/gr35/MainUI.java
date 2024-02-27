41c2f4bcb04c6d9f5b11bb2d3e0612c748c8e3d4
Brahim Helland <brahimh@stud.ntnu.no>
2024-02-27 15:29:46 +0100

    Updated pom.xml and added MainUI class so the project could be run with mvn javafx:run.

---
+++ src/main/java/edu/ntnu/stud/idatt2003/gr35/MainUI.java
@@ -0,0 +1,11 @@
+package edu.ntnu.stud.idatt2003.gr35;
+
+import javafx.application.Application;
+
+public class MainUI extends Application {
+    @Override
+    public void start(javafx.stage.Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World!");
+        primaryStage.show();
+    }
+}
