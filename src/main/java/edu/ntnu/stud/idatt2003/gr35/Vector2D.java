d5f9ed9c13aa6c34686d7fd73a2837be99596bfb
Wizard764 <150451202+Wizard764@users.noreply.github.com>
2024-03-06 12:42:48 +0100

    Added javadoc to Vector2D and corrected minor typo in method name.

--- src/main/java/edu/ntnu/stud/idatt2003/gr35/Vector2D.java
+++ src/main/java/edu/ntnu/stud/idatt2003/gr35/Vector2D.java
@@ -1,9 +1,19 @@
 package edu.ntnu.stud.idatt2003.gr35;
 
+/**
+ * A class representing a 2D vector.
+ */
 public class Vector2D {
+    // The elements of the vector
     final private double x0;
     final private double x1;
 
+    /**
+     * Constructor for the Vector2D class.
+     *
+     * @param x0 The element in the first position.
+     * @param x1 The element in the second position.
+     */
     public Vector2D(double x0, double x1) {
         this.x0 = x0;
         this.x1 = x1;
@@ -23,7 +33,7 @@ public class Vector2D {
         return new Vector2D(x0, x1);
     }
 
-    public Vector2D substract(Vector2D other) {
+    public Vector2D subtract(Vector2D other) {
         double x0 = this.x0 - other.getx0();
         double x1 = this.x1 - other.getx1();
         return new Vector2D(x0, x1);
