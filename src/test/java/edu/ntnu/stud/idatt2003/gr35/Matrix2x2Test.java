1f4d448d34d6402cc17b1e3fa56db02e8fe3e762
Wizard764 <150451202+Wizard764@users.noreply.github.com>
2024-03-06 12:44:34 +0100

    Added JUnit tests to Matrix2x2Test.

--- src/test/java/edu/ntnu/stud/idatt2003/gr35/Matrix2x2Test.java
+++ src/test/java/edu/ntnu/stud/idatt2003/gr35/Matrix2x2Test.java
@@ -1,4 +1,20 @@
 package edu.ntnu.stud.idatt2003.gr35;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Test class for the Matrix2x2 class.
+ */
 public class Matrix2x2Test {
+@Test
+  @DisplayName("Test multiply method")
+  void testMultiply() {
+    Matrix2x2 matrix = new Matrix2x2(1, 1, 1, 1);
+    Vector2D vector = new Vector2D(1, 1);
+    Vector2D vector2 = matrix.multiply(vector);
+    assertEquals(2, vector2.getx0());
+    assertEquals(2, vector2.getx1());
+  }
 }
