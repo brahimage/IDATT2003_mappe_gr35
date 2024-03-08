b6424287b4dbd7640d8fe698c3405861d30e1689
Wizard764 <150451202+Wizard764@users.noreply.github.com>
2024-03-08 12:19:20 +0100

    Add test methods in AffineTransform2DTest and JuliaTransformTest.

--- src/test/java/edu/ntnu/stud/idatt2003/gr35/model/transformations/AffineTransform2DTest.java
+++ src/test/java/edu/ntnu/stud/idatt2003/gr35/model/transformations/AffineTransform2DTest.java
@@ -1,5 +1,20 @@
 package edu.ntnu.stud.idatt2003.gr35.model.transformations;
 
-public class AffineTransform2DTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import edu.ntnu.stud.idatt2003.gr35.model.math.Matrix2x2;
+import edu.ntnu.stud.idatt2003.gr35.model.math.Vector2D;
+import org.junit.jupiter.api.Test;
+
+public class AffineTransform2DTest {
+  @Test
+  void testTransform() {
+    Vector2D vectorTrans = new Vector2D(.25, .5);
+    Matrix2x2 matrix = new Matrix2x2(0.5, 0, 0, 0.5);
+    AffineTransform2D transform = new AffineTransform2D(matrix, vectorTrans);
+
+    Vector2D vector = new Vector2D(1, 1);
+    Vector2D vector2 = transform.transform(vector);
+    assertEquals(0.75, vector2.getx0());
+  }
 }
--- src/test/java/edu/ntnu/stud/idatt2003/gr35/model/transformations/JuliaTransformTest.java
+++ src/test/java/edu/ntnu/stud/idatt2003/gr35/model/transformations/JuliaTransformTest.java
@@ -1,5 +1,18 @@
 package edu.ntnu.stud.idatt2003.gr35.model.transformations;
 
-public class JuliaTransformTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import edu.ntnu.stud.idatt2003.gr35.model.math.Sign;
+import edu.ntnu.stud.idatt2003.gr35.model.math.Vector2D;
+import org.junit.jupiter.api.Test;
+import edu.ntnu.stud.idatt2003.gr35.model.math.Complex;
+
+public class JuliaTransformTest {
+  @Test
+  void testTransform() {
+    JuliaTransform transform = new JuliaTransform(new Complex(1, 2), Sign.POSITIVE);
+    Vector2D vector = new Vector2D(1, 1);
+    Vector2D vector2 = transform.transform(vector);
+    assertEquals(0.7071067811865476, vector2.getx0());
+  }
 }
