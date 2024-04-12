d2de8c66d651fda6aa2ac09cbff10efefb21ec1e
Wizard764 <150451202+Wizard764@users.noreply.github.com>
2024-04-12 11:04:45 +0200

    Implemented writeToFile and tested.

--- src/main/java/edu/ntnu/stud/idatt2003/gr35/model/gameLogic/ChaosGameFileHandlerLEGACY.java
+++ src/main/java/edu/ntnu/stud/idatt2003/gr35/model/gameLogic/ChaosGameFileHandlerLEGACY.java
@@ -1,9 +1,46 @@
 package edu.ntnu.stud.idatt2003.gr35.model.gameLogic;
 
+import edu.ntnu.stud.idatt2003.gr35.model.transformations.AffineTransform2D;
+import edu.ntnu.stud.idatt2003.gr35.model.transformations.JuliaTransform;
+import edu.ntnu.stud.idatt2003.gr35.model.transformations.Transform2D;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 /**
  * Class for deconstructing and reconstructing ChaosGameDescription objects to and from files.
  * Uses 'manual' string parsing to read and write objects.
  */
 public class ChaosGameFileHandlerLEGACY {
   // To be implemented
+  public static void writeToFile(ChaosGameDescription description, String path) {
+    ArrayList<String> lines = new ArrayList<>(){{add ("");}};
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+      Transform2D sample = description.getTransforms().get(0);
+      lines.add(description.getMincoords().getx0() + ", " + description.getMincoords().getx1());
+      lines.add(description.getMaxcoords().getx0() + ", " + description.getMaxcoords().getx1());
+      if (sample instanceof AffineTransform2D) {
+        lines.set(0, "Affine2D");
+        List<Transform2D> transforms = description.getTransforms();
+        for (Transform2D transform : transforms) {
+          lines.add(((AffineTransform2D) transform).getTransformationAsString());
+        }
+      }
+      else if (sample instanceof JuliaTransform) {
+        lines.set(0, "Julia");
+        lines.add(((JuliaTransform) sample).getPointAsString());
+      }
+      else {
+        throw new Error("Unsupported transformation type: " + sample.getClass().getName());
+      }
+      for (String line : lines) {
+        writer.write(line);
+        writer.newLine();
+      }
+      writer.flush();
+    } catch (Exception e) {
+      System.out.println("An error occurred while writing to the file: " + e.getMessage());
+    }
+  }
 }
--- src/test/java/edu/ntnu/stud/idatt2003/gr35/model/gameLogic/ChaosGameFileHandlerLEGACYTest.java
+++ src/test/java/edu/ntnu/stud/idatt2003/gr35/model/gameLogic/ChaosGameFileHandlerLEGACYTest.java
@@ -1,8 +1,45 @@
 package edu.ntnu.stud.idatt2003.gr35.model.gameLogic;
 
+import edu.ntnu.stud.idatt2003.gr35.model.math.Complex;
+import edu.ntnu.stud.idatt2003.gr35.model.math.Matrix2x2;
+import edu.ntnu.stud.idatt2003.gr35.model.math.Sign;
+import edu.ntnu.stud.idatt2003.gr35.model.math.Vector2D;
+import edu.ntnu.stud.idatt2003.gr35.model.transformations.AffineTransform2D;
+import edu.ntnu.stud.idatt2003.gr35.model.transformations.JuliaTransform;
+import edu.ntnu.stud.idatt2003.gr35.model.transformations.Transform2D;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 /**
  * Test class for the ChaosGameFileHandlerLEGACY class.
  */
 public class ChaosGameFileHandlerLEGACYTest {
+  @Test
+  void testSaveChaosGameDescriptionAffine() {
+    // Test data for the ChaosGameDescription
+    Vector2D min = new Vector2D(0, 0);
+    Vector2D max = new Vector2D(1, 1);
+    List<Transform2D> transforms = new ArrayList<>();
+    // Add the 3 transformations that produce the Sierpinski triangle (they all have the same matrix)
+    Matrix2x2 sierpinskiMatrix = new Matrix2x2(.5, 0, 0, .5);
+    transforms.add(new AffineTransform2D(sierpinskiMatrix, new Vector2D(0, 0)));
+    transforms.add(new AffineTransform2D(sierpinskiMatrix, new Vector2D(.5, 0)));
+    transforms.add(new AffineTransform2D(sierpinskiMatrix, new Vector2D(.25, .5)));
 
+    ChaosGameDescription description = new ChaosGameDescription(min, max, transforms);
+    ChaosGameFileHandlerLEGACY.writeToFile(description, "ChaosGames/testSierpinski.txt");
+  }
+
+  @Test
+  void testSaveChaosGameDescriptionJulia() {
+    // Test data for the ChaosGameDescription
+    Vector2D min = new Vector2D(0, 0);
+    Vector2D max = new Vector2D(1, 1);
+    List<Transform2D> transforms = new ArrayList<>();
+    // Add the 3 transformations that produce the Sierpinski triangle (they all have the same matrix)
+    transforms.add(new JuliaTransform(new Complex(1, 2), Sign.POSITIVE));
+    ChaosGameDescription description = new ChaosGameDescription(min, max, transforms);
+    ChaosGameFileHandlerLEGACY.writeToFile(description, "ChaosGames/testJulia.txt");
+  }
 }
