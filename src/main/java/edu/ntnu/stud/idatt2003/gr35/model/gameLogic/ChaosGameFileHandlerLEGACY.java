d7a092377807cecc1e2f0135a0a6b2ff21156663
Wizard764 <150451202+Wizard764@users.noreply.github.com>
2024-04-12 10:04:48 +0200

    Added ChaosGameFileHandlerLEGACY class.

---
+++ src/main/java/edu/ntnu/stud/idatt2003/gr35/model/gameLogic/ChaosGameFileHandlerLEGACY.java
@@ -0,0 +1,9 @@
+package edu.ntnu.stud.idatt2003.gr35.model.gameLogic;
+
+/**
+ * Class for deconstructing and reconstructing ChaosGameDescription objects to and from files.
+ * Uses 'manual' string parsing to read and write objects.
+ */
+public class ChaosGameFileHandlerLEGACY {
+  // To be implemented
+}
