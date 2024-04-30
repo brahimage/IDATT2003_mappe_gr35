d822eff4627cb8baabb152b55998bee1f02dc3db
Wizard764 <150451202+Wizard764@users.noreply.github.com>
2024-04-30 14:47:56 +0200

    Added missing javadoc.

--- src/main/java/edu/ntnu/stud/idatt2003/gr35/controller/ChaosGameObserver.java
+++ src/main/java/edu/ntnu/stud/idatt2003/gr35/controller/ChaosGameObserver.java
@@ -1,5 +1,9 @@
 package edu.ntnu.stud.idatt2003.gr35.controller;
 
+/**
+ * Interface for classes that observe the ChaosGame class.
+
+ */
 public interface ChaosGameObserver {
     void update(Object o);
 }
