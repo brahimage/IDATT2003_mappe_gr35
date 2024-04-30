bc075434d978d478f4886162a03c8f681387446b
Wizard764 <150451202+Wizard764@users.noreply.github.com>
2024-04-30 14:42:53 +0200

    Added missing javadoc.

--- src/main/java/edu/ntnu/stud/idatt2003/gr35/MainUI.java
+++ src/main/java/edu/ntnu/stud/idatt2003/gr35/MainUI.java
@@ -2,6 +2,10 @@ package edu.ntnu.stud.idatt2003.gr35;
 
 import javafx.application.Application;
 
+/**
+ * The main class for the application.
+
+ */
 public class MainUI extends Application {
     @Override
     public void start(javafx.stage.Stage primaryStage) throws Exception {
