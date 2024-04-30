07cf1741be083860dfcb0aa700bcf0b22f6e7a6d
Wizard764 <150451202+Wizard764@users.noreply.github.com>
2024-04-30 14:50:11 +0200

    Added missing javadoc.

--- src/main/java/edu/ntnu/stud/idatt2003/gr35/model/gameLogic/ChaosGameDescriptionFactory.java
+++ src/main/java/edu/ntnu/stud/idatt2003/gr35/model/gameLogic/ChaosGameDescriptionFactory.java
@@ -1,4 +1,9 @@
 package edu.ntnu.stud.idatt2003.gr35.model.gameLogic;
 
+/**
+ * Represents a factory for creating ChaosGameDescription objects.
+ * Used to generate some basic chaos game descriptions for testing purposes.
+ */
 public class ChaosGameDescriptionFactory {
+
 }
