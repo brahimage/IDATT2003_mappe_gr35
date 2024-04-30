fbd7763020267fb253eb789241714af9360025bf
Wizard764 <150451202+Wizard764@users.noreply.github.com>
2024-04-30 15:04:40 +0200

    Added missing javadoc.

--- src/main/java/edu/ntnu/stud/idatt2003/gr35/view/gui/pageswitchbuttons/VariablePopUpButton.java
+++ src/main/java/edu/ntnu/stud/idatt2003/gr35/view/gui/pageswitchbuttons/VariablePopUpButton.java
@@ -3,14 +3,21 @@ package edu.ntnu.stud.idatt2003.gr35.view.gui.pageswitchbuttons;
 import edu.ntnu.stud.idatt2003.gr35.view.gui.pages.VariablePagePopUp;
 import javafx.scene.control.Button;
 
+/**
+ * A button that opens a pop-up window for adding a new variable.
+ */
 public class VariablePopUpButton extends Button {
 
+  /**
+   * Constructs a new VariablePopUpButton.
+   */
   public VariablePopUpButton() {
     super();
 
     this.setText("Variable PopUp");
     this.setId("small-button");
 
+    // Opens a new pop-up window for adding a new variable.
     this.setOnAction(e -> {
       new VariablePagePopUp().showPopUp();
     });
