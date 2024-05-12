b8be3b21d061a8ce3b23fe56bf3ae48c015bbbec
Wizard764 <jonasbau@stud.ntnu.no>
2024-05-12 20:02:00 +0200

    MainUI implements observer.

--- src/main/java/edu/ntnu/stud/idatt2003/gr35/MainUI.java
+++ src/main/java/edu/ntnu/stud/idatt2003/gr35/MainUI.java
@@ -1,15 +1,21 @@
 package edu.ntnu.stud.idatt2003.gr35;
 
 import edu.ntnu.stud.idatt2003.gr35.controller.SceneController;
+import edu.ntnu.stud.idatt2003.gr35.model.gameLogic.ChaosGameDescription;
 import edu.ntnu.stud.idatt2003.gr35.model.gameLogic.ChaosGameDescriptionFactory;
+import edu.ntnu.stud.idatt2003.gr35.model.gameLogic.ChaosGameFileHandler;
+import edu.ntnu.stud.idatt2003.gr35.model.math.Vector2D;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import java.util.Observable;
+import java.util.Observer;
 
 /**
  * The main class for the application.
 
  */
 public class MainUI extends Application {
+public class MainUI extends Application implements Observer {
     @Override
     public void start(Stage primaryStage) throws Exception {
         ChaosGameDescriptionFactory.GenerateTestFiles();
@@ -20,5 +26,16 @@ public class MainUI extends Application {
 
         sceneController.showViewPage();
         primaryStage.show();
+    }
+
+    /**
+     * update method called when play button is pressed.
+     *
+     * @param o     the observable object.
+     * @param arg   an argument passed to the {@code notifyObservers}
+     *                 method.
+     */
+    @Override
+    public void update(Observable o, Object arg) {
     }
 }
