b6e1c415b3f2f2d645101797b97ccbbf4f764e33
Wizard764 <jonasbau@stud.ntnu.no>
2024-05-12 19:56:17 +0200

    Added variable to track canvas dimensions.

--- src/main/java/edu/ntnu/stud/idatt2003/gr35/controller/SceneController.java
+++ src/main/java/edu/ntnu/stud/idatt2003/gr35/controller/SceneController.java
@@ -1,10 +1,21 @@
 package edu.ntnu.stud.idatt2003.gr35.controller;
 
+import edu.ntnu.stud.idatt2003.gr35.ChaosGame;
+import edu.ntnu.stud.idatt2003.gr35.model.math.Vector2D;
+import edu.ntnu.stud.idatt2003.gr35.view.gui.buttons.PlayButton;
 import edu.ntnu.stud.idatt2003.gr35.view.gui.pages.ViewPage;
 import java.io.FileNotFoundException;
 import java.util.Objects;
+import java.util.Observable;
+import java.util.Observer;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -15,6 +26,8 @@ public class SceneController {
   private final Scene scene;
   // The view page to be displayed.
   private final ViewPage viewPage;
+  // The dimensions of the canvas.
+  private Vector2D canvasDimensions;
 
   /**
    * Constructor for the SceneController.
@@ -31,6 +44,15 @@ public class SceneController {
 
     primaryStage.setScene(scene);
     this.viewPage = new ViewPage();
+  }
+
+  /**
+   * Gets the dimensions of the canvas.
+   *
+   * @return The dimensions of the canvas
+   */
+  public Vector2D getCanvasDimensions() {
+    return canvasDimensions;
   }
 
   /**
